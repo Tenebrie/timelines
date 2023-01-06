95f95cfc955af8d1db3d19211c048a96aae62619
Aleksandr Semjonov <kos94ok@gmail.com>
2023-01-06 21:30:53 +0200

    Cleanup Rhea

--- app/rhea-backend/src/routers/UserRouter.ts
+++ app/rhea-backend/src/routers/UserRouter.ts
@@ -1,43 +1,5 @@
-import {
-	BooleanValidator,
-	FooBarObjectValidator,
-	OptionalParam,
-	RequiredParam,
-	Router,
-	StringValidator,
-	usePathParams,
-	useQueryParams,
-	useRequestBody,
-	useRequestHeaders,
-	useRequestRawBody,
-} from 'tenebrie-framework'
+import { Router } from 'tenebrie-framework'
 
 const router = new Router()
-
-// router.post('/users', (ctx) => {
-// 	useApiEndpoint({
-// 		name: 'createUser',
-// 		description: 'Creates a user',
-// 		summary: 'Short summary',
-// 	})
-
-// 	const body = useRequestBody(ctx, {
-// 		email: NonEmptyString,
-// 		username: NonEmptyString,
-// 		password: NonEmptyString,
-// 	})
-
-// 	body.password
-
-// 	return body
-// })
-
-router.get('/test', (ctx) => {
-	useRequestHeaders(ctx, {
-		Authorization: RequiredParam({
-			rehydrate: (v) => v,
-		}),
-	})
-})
 
 export const UserRouter = router
