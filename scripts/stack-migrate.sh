1ea59c9296015b3a77e51d0e785d989aada2e8bd
Maya Lintunokka <kos94ok@gmail.com>
2023-04-03 01:08:57 +0300

    Fix script

--- scripts/stack-migrate.sh
+++ scripts/stack-migrate.sh
@@ -1,2 +1,3 @@
 mkdir -p /mnt/volume_rhea_postgres/data
-docker exec -it $(docker ps -qf "name=timelines_rhea.1") (DATABASE_URL=postgresql://docker:docker@rhea-postgres:5432/db?schema=public yarn prisma migrate deploy)
+docker exec -it $(docker ps -qf "name=timelines_rhea.1") bash
+DATABASE_URL=postgresql://docker:docker@rhea-postgres:5432/db?schema=public yarn prisma migrate deploy
