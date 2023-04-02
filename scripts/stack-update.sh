2882e78eaee5707ea69795051129537fa46d34cf
Maya Lintunokka <kos94ok@gmail.com>
2023-04-03 02:52:02 +0300

    Fix dockerfiles

---
+++ scripts/stack-update.sh
@@ -0,0 +1,3 @@
+docker service update --image 127.0.0.1:5000/gatekeeper timelines_gatekeeper
+docker service update --image 127.0.0.1:5000/rhea timelines_rhea
+docker service update --image 127.0.0.1:5000/styx timelines_styx
