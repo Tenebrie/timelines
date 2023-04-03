750d3571d245bccd0397d0eb59935661f725e5a1
Maya Lintunokka <kos94ok@gmail.com>
2023-04-03 22:25:29 +0300

    Switch to test cert

--- app/gatekeeper-proxy/scripts/init-certbot.sh
+++ app/gatekeeper-proxy/scripts/init-certbot.sh
@@ -2,4 +2,5 @@ certbot --nginx --non-interactive --agree-tos \
 	--no-eff-email \
 	--no-redirect \
 	--email 'kos94ok@gmail.com' \
-	--domains "timelines.tenebrie.com t1.tenebrie.com"
+	--domains "timelines.tenebrie.com t1.tenebrie.com" \
+	--test-cert
