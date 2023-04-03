550b7ee0681db8a407bdd4de778fed9c92d24bc7
Maya Lintunokka <kos94ok@gmail.com>
2023-04-03 22:11:51 +0300

    Improve Gatekeeper

---
+++ app/gatekeeper-proxy/scripts/init-certbot.sh
@@ -0,0 +1,5 @@
+certbot --nginx --non-interactive --agree-tos \
+	--no-eff-email \
+	--no-redirect \
+	--email 'kos94ok@gmail.com' \
+	--domains "timelines.tenebrie.com"
---
+++ app/gatekeeper-proxy/scripts/install-certbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update
+apt install python3 python3-venv libaugeas0 -y
+python3 -m venv /opt/certbot/
+/opt/certbot/bin/pip install --upgrade pip
+/opt/certbot/bin/pip install certbot certbot-nginx
+ln -s /opt/certbot/bin/certbot /usr/bin/certbot
