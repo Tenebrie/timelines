bc602e995d5357a90800f1bf0be38ac9971dc634
Maya Lintunokka <kos94ok@gmail.com>
2023-04-03 03:19:11 +0300

    Force slow builds

--- scripts/stack-build.sh
+++ scripts/stack-build.sh
@@ -1,2 +1,4 @@
+docker compose -f docker-compose.common.yml -f docker-stack.yml build styx
+docker compose -f docker-compose.common.yml -f docker-stack.yml build rhea
 docker compose -f docker-compose.common.yml -f docker-stack.yml build
 docker compose -f docker-compose.common.yml -f docker-stack.yml push
