ba299d14e072f74bfe8c1f399557f8e559675090
Maya Lintunokka <kos94ok@gmail.com>
2023-04-06 02:48:47 +0300

    Fix update script

--- scripts/stack-build.sh
+++
@@ -1,4 +0,0 @@
-docker compose -f docker-compose.common.yml -f docker-compose.prod.yml build styx
-docker compose -f docker-compose.common.yml -f docker-compose.prod.yml build rhea
-docker compose -f docker-compose.common.yml -f docker-compose.prod.yml build
-docker compose -f docker-compose.common.yml -f docker-compose.prod.yml push
--- scripts/stack-update.sh
+++ scripts/stack-update.sh
@@ -1,7 +1,3 @@
-docker service scale timelines_rhea=2
 docker service update --image tenebrie/timelines-rhea --update-delay 30s timelines_rhea
-docker service scale timelines_rhea=1
-
-docker service scale timelines_styx=2
 docker service update --image tenebrie/timelines-styx --update-delay 30s timelines_styx
-docker service scale timelines_styx=1
+docker service update --image tenebrie/timelines-calliope --update-delay 30s timelines_calliope
