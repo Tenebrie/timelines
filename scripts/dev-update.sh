569238f148dded519069ea96664199772b163de7
Maija Lintunokka <maija.lintunokka@gmail.com>
2023-06-21 00:30:13 +0300

    Fix update script

--- scripts/dev-update.sh
+++ scripts/dev-update.sh
@@ -7,5 +7,5 @@ yarn
 docker compose -f docker-compose.common.yml -f docker-compose.dev.yml stop
 docker compose -f docker-compose.common.yml -f docker-compose.dev.yml build
 docker compose -f docker-compose.common.yml -f docker-compose.dev.yml up -d --wait
-(cd app/rhea-backend && yarn prisma migrate dev)
+(cd app/rhea-backend && yarn prisma:migrate:dev)
 docker compose -f docker-compose.common.yml -f docker-compose.dev.yml up
