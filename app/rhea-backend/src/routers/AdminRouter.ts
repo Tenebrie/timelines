180773fa48d23b5ab87a9ac581c8ae1f177674fb
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-01-08 03:30:20 +0200

    chore: use framework 0.7.1 syntax in admin router

--- app/rhea-backend/src/routers/AdminRouter.ts
+++ app/rhea-backend/src/routers/AdminRouter.ts
@@ -2,18 +2,21 @@ import { AdminAuthenticator } from '@src/auth/AdminAuthenticator'
 import { AdminService } from '@src/services/AdminService'
 import { Router, useApiEndpoint, useAuth } from 'tenebrie-framework'
 
-const router = new Router()
+const router = new Router().with(async (ctx) => {
+	const user = await useAuth(ctx, AdminAuthenticator)
+	return {
+		user,
+	}
+})
 
 export const adminUsersTag = 'adminUsers'
 
-router.get('/api/admin/users', async (ctx) => {
+router.get('/api/admin/users', async () => {
 	useApiEndpoint({
 		name: 'adminGetUsers',
 		description: 'Gets list of all registered users',
 		tags: [adminUsersTag],
 	})
-
-	await useAuth(ctx, AdminAuthenticator)
 
 	const users = await AdminService.listUsers()
 
--- app/rhea-backend/src/services/AdminService.ts
+++ app/rhea-backend/src/services/AdminService.ts
@@ -2,6 +2,13 @@ import { dbClient } from './DatabaseClient'
 
 export const AdminService = {
 	listUsers: async () => {
-		return dbClient.user.findMany({})
+		return dbClient.user.findMany({
+			select: {
+				id: true,
+				email: true,
+				level: true,
+				username: true,
+			},
+		})
 	},
 }
