bc0e7ded74227887da30b21a3fb3e3efd02b5acd
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-10-24 21:41:31 +0300

    fix: add semver action

--- scripts/stack-deploy.sh
+++ scripts/stack-deploy.sh
@@ -1,1 +1,7 @@
+if [[ -z "$VERSION" ]]; then
+    echo "Must provide VERSION environment" 1>&2
+    exit 1
+fi
+
+
 docker stack deploy -c docker-compose.common.yml -c docker-compose.prod.yml timelines
--- scripts/stack-update.sh
+++ scripts/stack-update.sh
@@ -1,3 +1,8 @@
+if [[ -z "$VERSION" ]]; then
+    echo "Must provide VERSION environment" 1>&2
+    exit 1
+fi
+
 docker service update --image tenebrie/timelines-rhea:${VERSION} --update-delay 30s timelines_rhea
 docker service update --image tenebrie/timelines-styx:${VERSION} --update-delay 30s timelines_styx
 docker service update --image tenebrie/timelines-calliope:${VERSION} --update-delay 30s timelines_calliope
