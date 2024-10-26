09defeafb8ea72583ed06f8e11870dce60b5766d
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-10-27 00:11:11 +0300

    feat: gatekeeper improvements

--- app/gatekeeper-proxy/scripts/install-certbot.sh
+++ app/gatekeeper-proxy/scripts/install-certbot.sh
@@ -6,3 +6,4 @@ python3 -m venv /opt/certbot/
 /opt/certbot/bin/pip install --upgrade pip
 /opt/certbot/bin/pip install certbot certbot-nginx
 ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null
