22a66f798dab1132deecfbcd7b501905114ab926
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-10-27 14:15:43 +0200

    fix: switch to prod SSL cert

--- app/gatekeeper-proxy/scripts/init-certbot.sh
+++ app/gatekeeper-proxy/scripts/init-certbot.sh
@@ -1,8 +1,7 @@
 certbot --nginx --non-interactive --agree-tos \
 	--no-eff-email \
 	--email 'kos94ok@gmail.com' \
-	--domains "timelines.tenebrie.com" \
-	--test-cert
+	--domains "timelines.tenebrie.com"
 
 service nginx status
 service nginx stop
