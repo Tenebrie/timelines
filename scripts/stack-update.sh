abcc32d15b1904478933da42ded821bd68a6f2dc
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-10-30 01:01:57 +0200

    fix: event creation

--- scripts/stack-update.sh
+++ scripts/stack-update.sh
@@ -3,8 +3,8 @@ if [[ -z "$VERSION" ]]; then
     exit 1
 fi
 
-docker service update --image tenebrie/timelines-gatekeeper:${VERSION} --update-delay 60s --update-parallelism 1 --update-failure-action rollback timelines_gatekeeper
-docker service update --image tenebrie/timelines-rhea:${VERSION} --update-delay 60s --update-parallelism 1 --update-failure-action rollback timelines_rhea
-docker service update --image tenebrie/timelines-calliope:${VERSION} --update-delay 60s --update-parallelism 1 --update-failure-action rollback timelines_calliope
-docker service update --image tenebrie/timelines-styx:${VERSION} --update-delay 60s --update-parallelism 1 --update-failure-action rollback timelines_styx
+docker service update --image tenebrie/timelines-gatekeeper:${VERSION} --update-delay 5s --update-parallelism 1 --update-failure-action rollback timelines_gatekeeper
+docker service update --image tenebrie/timelines-rhea:${VERSION} --update-delay 5s --update-parallelism 1 --update-failure-action rollback timelines_rhea
+docker service update --image tenebrie/timelines-calliope:${VERSION} --update-delay 5s --update-parallelism 1 --update-failure-action rollback timelines_calliope
+docker service update --image tenebrie/timelines-styx:${VERSION} --update-delay 5s --update-parallelism 1 --update-failure-action rollback timelines_styx
 docker system prune -f
