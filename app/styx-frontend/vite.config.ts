011e29436bd81fc6b3cccf84a7af19c3edc8704a
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-10-30 01:04:36 +0200

    fix: undo build chunking

--- app/styx-frontend/vite.config.ts
+++ app/styx-frontend/vite.config.ts
@@ -13,47 +13,6 @@ export default defineConfig({
 	},
 	build: {
 		outDir: 'build',
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					const extractedDeps = [
-						'styled-components',
-						'react-dom',
-						'redux',
-						'@mui/system',
-						'@mui/material/A',
-						'@mui/material/B',
-						'@mui/material/C',
-						'@mui/material/E',
-						'@mui/material/J',
-						'@mui/material/K',
-						'@mui/material/Q',
-						'@mui/material/S',
-						'@mui/material/T',
-						'@mui/material/V',
-						'@mui/material/W',
-						'@mui/material/X',
-						'@mui/material/Y',
-						'@mui/material',
-						'@emotion',
-						'rollbar',
-						'zod',
-						'react',
-					]
-					for (let i = 0; i < extractedDeps.length; i++) {
-						if (id.includes(extractedDeps[i])) {
-							return extractedDeps[i].replace('@', '').replace('/', '-')
-						}
-					}
-					if (id.includes('node_modules')) {
-						return 'vendor'
-					} else if (id.includes('src/app/features/world')) {
-						return 'world'
-					}
-					return undefined
-				},
-			},
-		},
 	},
 	server: {
 		port: 8080,
