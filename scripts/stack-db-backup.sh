5f62e0bb99be1684b77f8896f81a6495565ea83f
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-11-02 22:28:33 +0200

    chore: add db backup into deployment

--- scripts/stack-db-backup.sh
+++ scripts/stack-db-backup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker exec -it $(docker ps -aqf "name=timelines_rhea-postgres") bash -c "pg_dump -Udocker db > /backups/db.sql"
+# Rename the file to include the current date
+mv /mnt/volume_rhea_postgres/backups/db.sql /mnt/volume_rhea_postgres/backups/db-$(date +%Y-%m-%d).sql
+# Delete files older than 7 days
+find /mnt/volume_rhea_postgres/backups/ -type f -name '*.sql' -mtime +7 -exec rm {} \;
