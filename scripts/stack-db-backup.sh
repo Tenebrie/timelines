18cb65a0c33015df274263828aeff5e7640afd59
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-11-02 22:30:45 +0200

    chore: fix db backup time

--- scripts/stack-db-backup.sh
+++ scripts/stack-db-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker exec -it $(docker ps -aqf "name=timelines_rhea-postgres") bash -c "pg_dump -Udocker db > /backups/db.sql"
-# Rename the file to include the current date
-mv /mnt/volume_rhea_postgres/backups/db.sql /mnt/volume_rhea_postgres/backups/db-$(date +%Y-%m-%d).sql
+# Rename the file to include the current date and time
+mv /mnt/volume_rhea_postgres/backups/db.sql /mnt/volume_rhea_postgres/backups/db-$(date +%Y-%m-%d_%H-%M).sql
 # Delete files older than 7 days
 find /mnt/volume_rhea_postgres/backups/ -type f -name '*.sql' -mtime +7 -exec rm {} \;
