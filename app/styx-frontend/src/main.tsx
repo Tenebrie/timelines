746f13c6010a69f6434c7e7da8dd1c8c798f6d0b
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-02-28 23:27:00 +0200

    chore: nuke rollbar

--- app/styx-frontend/src/main.tsx
+++ app/styx-frontend/src/main.tsx
@@ -1,11 +1,9 @@
 import './main.css'
 
-import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react'
 import { RouterProvider } from '@tanstack/react-router'
 import React, { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
-import { Configuration } from 'rollbar'
 
 import { useAuthCheck } from './app/features/auth/authCheck/useAuthCheck'
 import { store } from './app/store'
@@ -17,14 +15,6 @@ loadSafariPolyfills()
 const container = document.getElementById('root')
 const root = createRoot(container!)
 export const reactRoot = root
-
-const rollbarConfig: Configuration = {
-	enabled: import.meta.env.PROD,
-	accessToken: 'f426a5264860489095a22b56cc8d8e31',
-	environment: import.meta.env.MODE,
-	captureUncaught: true,
-	captureUnhandledRejections: true,
-}
 
 const RouterWrapper = () => {
 	const auth = useAuthCheck()
@@ -40,12 +30,8 @@ const RouterWrapper = () => {
 root.render(
 	<React.StrictMode>
 		<ReduxProvider store={store}>
-			<RollbarProvider config={rollbarConfig}>
-				<ErrorBoundary>
-					<RouterWrapper />
-					{/* <TanStackRouterDevtools router={router} />  */}
-				</ErrorBoundary>
-			</RollbarProvider>
+			<RouterWrapper />
+			{/* <TanStackRouterDevtools router={router} />  */}
 		</ReduxProvider>
 	</React.StrictMode>,
 )
