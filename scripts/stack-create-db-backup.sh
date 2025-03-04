a4e36f701a2868be29a416f32bfec73a0d6cc7b5
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-03-04 11:35:07 +0200

    chore: introduce gzip

--- scripts/stack-create-db-backup.sh
+++ scripts/stack-create-db-backup.sh
@@ -16,14 +16,16 @@ docker exec "$CONTAINER_ID" bash -c "pg_dump -Udocker db > /backups/db.sql"
 # Prep some variables
 DATE=$(date +%Y-%m-%d_%H-%M)
 BACKUP_PATH="/mnt/volume_rhea_postgres/backups/db.sql"
-UPLOAD_PATH="s3://$BUCKET/rhea/backups/db-$DATE.sql"
+UPLOAD_PATH="s3://$BUCKET/rhea/backups/db-$DATE.sql.gz"
+
+gzip -c "$BACKUP_PATH" > "$BACKUP_PATH.gz"
 
 # Sanity check that the backup file exists and is not empty
-if [ ! -s "$BACKUP_PATH" ]; then
+if [ ! -s "$BACKUP_PATH.gz" ]; then
   echo "Backup file does not exist or is empty."
   exit 1
 fi
 
 # Upload the backup to DigitalOcean Spaces
-aws s3 cp $BACKUP_PATH $UPLOAD_PATH --endpoint-url https://fra1.digitaloceanspaces.com
+aws s3 cp $BACKUP_PATH.gz $UPLOAD_PATH --endpoint-url https://fra1.digitaloceanspaces.com
 echo "Successfully uploaded the latest backup to $UPLOAD_PATH"
