5ccb43ec89ecf6066f85fad910e469dbdc5d96b3
Maija Lintunokka <maija.lintunokka@gmail.com>
2025-07-02 20:23:07 +0300

    fix: test

--- app/rhea-backend/src/utils/keysOf.ts
+++ app/rhea-backend/src/utils/keysOf.ts
@@ -1,3 +1,6 @@
-export const keysOf = <T extends string | number | symbol>(object: Record<T, unknown>) => {
+export const keysOf = <T extends string | number | symbol>(object: Record<T, unknown> | undefined) => {
+	if (!object) {
+		return []
+	}
 	return Object.keys(object) as (keyof typeof object)[]
 }
