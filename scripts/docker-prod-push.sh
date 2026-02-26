07c1fdbad0e5be7b646240b56d83e9378ed980a5
Maija Lintunokka <maija.lintunokka@nosto.com>
2026-02-26 15:52:18 +0200

    feat: add thetis to prod deployment

--- scripts/docker-prod-push.sh
+++ scripts/docker-prod-push.sh
@@ -7,5 +7,6 @@ docker push tenebrie/timelines-styx:${VERSION}
 docker push tenebrie/timelines-rhea:${VERSION}
 docker push tenebrie/timelines-calliope:${VERSION}
 docker push tenebrie/timelines-orpheus:${VERSION}
+docker push tenebrie/timelines-thetis:${VERSION}
 docker push tenebrie/timelines-chronos:${VERSION}
 echo "\033[38;5;154;48;5;22mPublish successful! Version: \033[0m\033[38;5;226;48;5;22m"${VERSION}"\033[0m"
--- scripts/stack-deploy.sh
+++ scripts/stack-deploy.sh
@@ -14,6 +14,7 @@ SERVICES=(
   "timelines_rhea"
   "timelines_calliope"
   "timelines_orpheus"
+  "timelines_thetis"
   "timelines_chronos"
 )
 
