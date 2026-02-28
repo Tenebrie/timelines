a7de17927537923c0d29480d8a713377783ca4d0
Maija Lintunokka <maija.lintunokka@gmail.com>
2026-02-28 16:30:08 +0200

    feat: time travel with events

--- app/styx-frontend/src/app/features/modals/ModalsRenderer.tsx
+++ app/styx-frontend/src/app/features/modals/ModalsRenderer.tsx
@@ -7,10 +7,10 @@ import { DeleteAccountModal } from '../../views/profile/modals/DeleteAccountModa
 import { DeleteAssetModal } from '../../views/profile/modals/DeleteAssetModal'
 import { ArticleWizardModal } from '../../views/world/views/wiki/modals/ArticleWizardModal'
 import { DeleteArticleModal } from '../../views/world/views/wiki/modals/DeleteArticleModal'
+import { TimeTravelModal } from '../time/timeTravel/TimeTravelModal'
 import { useModal } from './ModalsSlice'
 import { EventTrackEditModal } from './renderers/EventTrackEditModal'
 import { EventTrackWizardModal } from './renderers/EventTrackWizardModal'
-import { TimeTravelModal } from './renderers/TimeTravelModal'
 
 export const ModalsRenderer = () => {
 	const { isLoaded } = useSelector(getWorldState, (a, b) => a.id === b.id && a.isLoaded === b.isLoaded)
--- app/styx-frontend/src/app/features/time/timeTravel/TimeTravelModal.tsx
+++ app/styx-frontend/src/app/features/time/timeTravel/TimeTravelModal.tsx
@@ -16,25 +16,37 @@ import { useStableNavigate } from '@/router-utils/hooks/useStableNavigate'
 import Modal, { ModalFooter, ModalHeader, useModalCleanup } from '@/ui-lib/components/Modal'
 
 import { useEventBusDispatch } from '../../eventBus'
-import { useTimeSelector } from '../../time/hooks/useTimeSelector'
-import { useWorldTime } from '../../time/hooks/useWorldTime'
-import { useModal } from '../ModalsSlice'
-import { TimeTravelModalInfo } from './TimeTravelModalInfo'
+import { useModal } from '../../modals/ModalsSlice'
+import { TimeTravelModalInfo } from '../../modals/renderers/TimeTravelModalInfo'
+import { useTimeSelector } from '../hooks/useTimeSelector'
+import { useWorldTime } from '../hooks/useWorldTime'
+import { useMarkerTimeTravel } from './useMarkerTimeTravel'
 
 export const TimeTravelModal = () => {
 	const { isOpen, close } = useModal('timeTravelModal')
 	const navigate = useStableNavigate({ from: '/world/$worldId' })
 
-	const { selectedTime } = useSelector(getWorldState, (a, b) => a.selectedTime === b.selectedTime)
+	const { selectedTime, selectedTimelineMarkers } = useSelector(
+		getWorldState,
+		(a, b) => a.selectedTime === b.selectedTime && a.selectedTimelineMarkers === b.selectedTimelineMarkers,
+	)
 
 	const { timeToLabel, calendar } = useWorldTime()
 	const { applySelector } = useTimeSelector({ rawTime: selectedTime })
 
 	const calendarRef = useRef<WorldCalendar>(null)
 	const selectorRef = useRef<HTMLInputElement | null>(null)
+
 	const [timeSelector, setTimeSelector] = useState('')
+	const [moveMarkersValue, setMoveMarkersValue] = useState(true)
+
+	const [error, setError] = useState<string | null>(null)
+
+	const [initialTime, setInitialTime] = useState(selectedTime)
 	const [targetTime, setTargetTime] = useState(selectedTime)
 	const [displayedTargetTime, setDisplayedTargetTime] = useState('')
+
+	const [moveMarkers] = useMarkerTimeTravel()
 
 	useEffect(() => {
 		if (calendar !== calendarRef.current) {
@@ -49,6 +61,7 @@ export const TimeTravelModal = () => {
 			setTimeSelector(value)
 			const { timestamp } = applySelector(value)
 			setTargetTime(timestamp)
+			setError(null)
 
 			setDisplayedTargetTime(timeToLabel(timestamp))
 		},
@@ -60,6 +73,8 @@ export const TimeTravelModal = () => {
 		onCleanup: () => {
 			const { timestamp } = applySelector(timeSelector)
 			setTargetTime(timestamp)
+			setInitialTime(selectedTime)
+			setError(null)
 			setDisplayedTargetTime(timeToLabel(timestamp))
 		},
 	})
@@ -70,6 +85,16 @@ export const TimeTravelModal = () => {
 		if (!isOpen) {
 			return
 		}
+
+		if (selectedTimelineMarkers.length > 0 && moveMarkersValue) {
+			const result = moveMarkers(targetTime - initialTime)
+			console.log(targetTime - initialTime)
+			if (result.error) {
+				setError(result.error)
+				return
+			}
+		}
+
 		scrollTimelineTo({ timestamp: targetTime })
 		navigate({
 			search: (prev) => ({
@@ -77,8 +102,19 @@ export const TimeTravelModal = () => {
 				time: targetTime,
 			}),
 		})
+
 		close()
-	}, [isOpen, targetTime, scrollTimelineTo, navigate, close])
+	}, [
+		isOpen,
+		scrollTimelineTo,
+		targetTime,
+		navigate,
+		selectedTimelineMarkers.length,
+		moveMarkersValue,
+		close,
+		moveMarkers,
+		initialTime,
+	])
 
 	useEffect(() => {
 		const interval = window.setInterval(() => {
@@ -108,11 +144,23 @@ export const TimeTravelModal = () => {
 				value={timeSelector}
 				onChange={(event) => onSelectorChanged(event.target.value)}
 				autoFocus
+				helperText={error}
+				error={!!error}
 			/>
 			<Typography>
 				<b>Travel to:</b> {displayedTargetTime}
 			</Typography>
-			<FormControlLabel control={<Checkbox defaultChecked />} label="Move selected events" />
+			{selectedTimelineMarkers.length > 0 && (
+				<FormControlLabel
+					control={
+						<Checkbox
+							checked={moveMarkersValue}
+							onChange={(event) => setMoveMarkersValue(event.target.checked)}
+						/>
+					}
+					label="Move selected markers"
+				/>
+			)}
 			<Divider />
 			<TimeTravelModalInfo />
 			<ModalFooter>
