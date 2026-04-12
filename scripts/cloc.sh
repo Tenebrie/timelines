b61fb20ceaf637ca26d4fa3845a4d2367cda2cf9
Maija Lintunokka <maija.lintunokka@nosto.com>
2026-04-12 04:19:11 +0300

    fix: update cloc script

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -5,5 +5,5 @@ cd "${0%/*}"
 TOTAL='\033[0;36m'
 NC='\033[0m'
 echo -e "${TOTAL} [[Total]]"
-npx cloc ../app/styx-frontend/src/ ../app/rhea-backend/src/ ../app/calliope-websockets/src
+npx cloc ../app/styx-frontend/src/ ../app/rhea-backend/src/ ../app/calliope-websockets/src ../app/thetis-landing/src ../app/orpheus-mcp/src
 echo -e "${NC}"
